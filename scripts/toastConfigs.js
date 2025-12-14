0917fb02e50411159d712ddc79c139ec3fab8a79
jukalbf <lgbr212@gmail.com>
2025-12-14 18:21:16 -0300

    feat(toastConfigs): Adicionando toast de confirmação de assinatura

--- scripts/toastConfigs.js
+++ scripts/toastConfigs.js
@@ -23,3 +23,16 @@ export const toastSucessConfig = {
     background: "rgb(0, 255, 0)",
   },
 };
+
+export const toastSignSucess = {
+  text: "Assinatura salva.",
+  duration: 3000,
+  newWindow: true,
+  close: true,
+  gravity: "top",
+  position: "center",
+  stopOnFocus: false,
+  style: {
+    background: "rgb(0, 255, 0)",
+  },
+};
