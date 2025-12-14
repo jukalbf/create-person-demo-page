fdf8f1921692092b39b85ff3526f5ba90c777976
jukalbf <lgbr212@gmail.com>
2025-12-14 18:20:43 -0300

    feat(handleRegisterPersonFunction): Função para assinar canvas e salvar como pdf

--- scripts/handleRegisterPersonFunction.js
+++ scripts/handleRegisterPersonFunction.js
@@ -1,11 +1,23 @@
-import { toastErrorConfig, toastSucessConfig } from "./toastConfigs.js";
+import {
+  toastErrorConfig,
+  toastSignSucess,
+  toastSucessConfig,
+} from "./toastConfigs.js";
+import addNewPerson from "./addNewPersonFunction.js";
+
+const buttonSign = document.querySelector(".button-signed");
+const canvas = document.querySelector(".digital-signature-canvas");
 
 const nameInput = document.querySelector(".name-input");
 const emailInput = document.querySelector(".email-input");
 const cpfInput = document.querySelector(".cpf-input");
+var isSigned = false;
+var signatureImg = null;
+
+buttonSign.addEventListener("click", signSignature);
 
 function handleRegisterPerson() {
-  if (!nameInput.value || !emailInput.value || !cpfInput.value) {
+  if (!nameInput.value || !emailInput.value || !cpfInput.value || !isSigned) {
     Toastify(toastErrorConfig).showToast();
     return;
   }
@@ -16,9 +28,17 @@ function handleRegisterPerson() {
     cpf: cpfInput.value,
   };
 
-  addNewPerson(newPerson.name, newPerson.email, newPerson.cpf);
+  addNewPerson(newPerson.name, newPerson.email, newPerson.cpf, signatureImg);
+  console.log(signatureImg);
 
   Toastify(toastSucessConfig).showToast();
+}
+
+function signSignature() {
+  isSigned = true;
+  signatureImg = canvas.toDataURL("image/png");
+
+  Toastify(toastSignSucess).showToast();
 }
 
 export default handleRegisterPerson;
