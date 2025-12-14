3020e35dd298fedc94afa9326d09d1c4b39b42c3
jukalbf <lgbr212@gmail.com>
2025-12-14 16:03:25 -0300

    feat(canvas): Melhorando lápis do canvas

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -5,42 +5,51 @@ let mouseInitPosition = null;
 let stopMouseTracking = false;
 let mousePosX = null;
 let mousePosY = null;
+let startDrawing = false;
 
 canvas.addEventListener("mousemove", handleDrawTrackMouse);
 
+canvas.addEventListener("mousedown", () => {
+  startDrawing = true;
+});
+canvas.addEventListener("mouseleave", () => {
+  startDrawing = false;
+});
 // canvas.addEventListener("mousedown", handleMouseDownDraw);
 
 function handleDrawTrackMouse(event) {
-  const canvasRect = canvas.getBoundingClientRect();
-  mousePosX = event.clientX - canvasRect.left;
-  mousePosY = event.clientY - canvasRect.top;
-  let strokeColor = "#00000000";
+  if (startDrawing) {
+    const canvasRect = canvas.getBoundingClientRect();
+    mousePosX = event.clientX - canvasRect.left;
+    mousePosY = event.clientY - canvasRect.top;
+    let strokeColor = "#00000000";
 
-  if (!mouseInitPosition && !stopMouseTracking) {
-    mouseInitPosition = {
-      xInitPosition: event.clientX,
-      yInitPosition: event.clientY,
-    };
+    if (!mouseInitPosition && !stopMouseTracking) {
+      mouseInitPosition = {
+        xInitPosition: event.clientX,
+        yInitPosition: event.clientY,
+      };
+    }
+
+    stopMouseTracking = true;
+    ctx.beginPath(
+      mouseInitPosition.xInitPosition,
+      mouseInitPosition.yInitPosition
+    );
+    ctx.fillRect(mousePosX, mousePosY, 5, 5);
+
+    canvas.addEventListener("mousedown", function () {
+      strokeColor = "#000000";
+      ctx.fillStyle = strokeColor;
+      ctx.stroke();
+    });
+
+    canvas.addEventListener("mouseup", function () {
+      strokeColor = "#00000000";
+      ctx.fillStyle = strokeColor;
+      ctx.stroke();
+    });
   }
-
-  stopMouseTracking = true;
-  ctx.beginPath(
-    mouseInitPosition.xInitPosition,
-    mouseInitPosition.yInitPosition
-  );
-  ctx.fillRect(mousePosX, mousePosY, 5, 5);
-
-  canvas.addEventListener("mousedown", function () {
-    strokeColor = "#000000";
-    ctx.fillStyle = strokeColor;
-    ctx.stroke();
-  });
-
-  canvas.addEventListener("mouseup", function () {
-    strokeColor = "#00000000";
-    ctx.fillStyle = strokeColor;
-    ctx.stroke();
-  });
 }
 
 function handleMouseDownDraw() {}
